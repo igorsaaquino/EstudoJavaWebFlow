fd1998b1608c703fddd1d183df9217e491957f3c
igorsaaquino <igoralencar1985@gmail.com>
2024-03-26 08:13:13 -0300

    Initial commit

---
+++ src/main/java/com/igoralencardev/aula/AulaApplication.java
@@ -0,0 +1,43 @@
+package com.igoralencardev.aula;
+
+import com.igoralencardev.entities.Employee;
+import com.igoralencardev.services.PensionService;
+import com.igoralencardev.services.SalaryService;
+import com.igoralencardev.services.TaxService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class AulaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.print("Nome: ");
+		String name = sc.nextLine();
+		System.out.print("Salário Bruto: ");
+		double grossSalary = sc.nextDouble();
+
+		Employee employee = new Employee(name, grossSalary);
+		TaxService taxService = new TaxService();
+		PensionService pensionService = new PensionService();
+		SalaryService salaryService = new SalaryService(taxService, pensionService);
+
+		double netSalary = salaryService.netSalary(employee);
+		System.out.printf("Salario liquido = %.2f%n",netSalary);
+
+		sc.close();
+
+	}
+}
---
+++ src/main/java/com/igoralencardev/services/TaxService.java
@@ -0,0 +1,9 @@
+package com.igoralencardev.services;
+
+public class TaxService {
+
+    public double tax(double amount){
+        return amount * 0.2;
+    }
+}
+
