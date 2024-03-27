503c6455b7069f00b0ec2d5a7692dd99951c787d
igorsaaquino <igoralencar1985@gmail.com>
2024-03-26 21:17:31 -0300

    [FIX] BrazilianTax

--- src/main/java/com/igoralencardev/aula/AulaApplication.java
+++ src/main/java/com/igoralencardev/aula/AulaApplication.java
@@ -13,25 +13,29 @@ import java.util.Locale;
 import java.util.Scanner;
 
 @SpringBootApplication
-public class AulaApplication {
+public class AulaApplication implements CommandLineRunner {
 
-	public static void main() {
+	public static void main(String[] args) {
+		SpringApplication.run(AulaApplication.class, args);
+	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
-	Locale.setDefault(Locale.US);
-	Scanner sc = new Scanner(System.in);
 
 		System.out.print("Nome: ");
-	String name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Salário Bruto: ");
-	double grossSalary = sc.nextDouble();
+		double grossSalary = sc.nextDouble();
 
-	Employee employee = new Employee(name, grossSalary);
-	BrazilTaxService brazilTaxService = new BrazilTaxService();
-	PensionService pensionService = new PensionService();
-	SalaryService salaryService = new SalaryService(brazilTaxService, pensionService);
+		Employee employee = new Employee(name, grossSalary);
+		BrazilTaxService brazilTaxService = new BrazilTaxService();
+		PensionService pensionService = new PensionService();
+		SalaryService salaryService = new SalaryService(brazilTaxService, pensionService);
 
-	double netSalary = salaryService.netSalary(employee);
+		double netSalary = salaryService.netSalary(employee);
 		System.out.printf("Salario liquido = %.2f%n",netSalary);
 
 		sc.close();
